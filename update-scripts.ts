3d62d19497ee540ffe42cd636d237b4ecf5e695e
wleklinskimateusz <wleklinskimateusz05@gmail.com>
2024-07-30 21:45:11 +0200

    chore: update scripts and remove unknown types

--- update-scripts.ts
+++ update-scripts.ts
@@ -1,13 +1,16 @@
 import { $, Glob } from "bun";
+import { join } from "path";
 
 const HOME = process.env.HOME;
 const ALIASES = `${HOME}/.bash_aliases`;
+// create aliases file if it doesn't exist
+await $`touch ${ALIASES}`;
 
 await $`sed -i '/# SCRIPTS START/,/# SCRIPTS END/d' ${ALIASES}`;
 
 await $`echo "# SCRIPTS START" >> ${ALIASES}`;
 
-const glob = new Glob(`${HOME}/Documents/scripts/*`);
+const glob = new Glob(join(import.meta.dir, "*"));
 
 for (const f of glob.scanSync(".")) {
   if (!f.endsWith(".ts")) {
